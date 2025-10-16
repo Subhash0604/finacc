b832d5b8e8d4ee832ef2df9cdaf3beb854e411e3
Subhash0604 <subhashchandra26545@gmail.com>
2025-10-16 09:43:07 +0530

    added account specific card

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -45,7 +45,7 @@ const AccountCard = ({ account }) => {
     return (
         <div>
             <Card className="hover:shadow-md transition-shadow cursor-pointer group relatives">
-                <Link href={`/account/${id}`}>
+                <Link href={`/accounts/${id}`}>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium capitalize">{name}</CardTitle>
                     <Switch checked={isDefault} className='cursor-pointer' onClick={handleDefaultChange} disabled={updateDefaultLoading}/>
