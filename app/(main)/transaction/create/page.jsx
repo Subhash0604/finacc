c7483eaef8928ca1a7bec268c405b7b87e2e77d3
Subhash Peddireddy <138444934+Subhash0604@users.noreply.github.com>
2025-10-31 09:35:40 +0530

    create transaction form

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -7,7 +7,7 @@ import TransactionForm from '../_components/transactionForm';
   const accounts = await getAccounts();
 
   return (
-    <div className="max-w-3xl mx-auto px-5">
+    <div className="max-w-3xl mx-auto px-5"> 
       <h1 className="gradient-title text-5xl mb-8">
         Add Transaction
       </h1>
