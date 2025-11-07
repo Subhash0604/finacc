fc16dc7feee9c0999bc30b1d0af00b5e8a39ad14
Subhash Peddireddy <138444934+Subhash0604@users.noreply.github.com>
2025-11-07 10:45:13 +0530

    fix issue

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -7,6 +7,7 @@ import { TransactionForm } from '../_components/transactionForm';
  export default async function CreateAcc() {
   const accounts = await getAccounts();
 
+  
   return (
     <div className="max-w-3xl mx-auto px-5"> 
       <h1 className="gradient-title text-5xl mb-8">
