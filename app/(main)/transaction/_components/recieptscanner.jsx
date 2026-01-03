6d35354d3f5a163e4325c45e62997c31e7129c3b
Subhash0604 <subhashchandra26545@gmail.com>
2026-01-03 08:59:10 +0530

    add reciept scanner component

--- app/(main)/transaction/_components/recieptscanner.jsx
+++ app/(main)/transaction/_components/recieptscanner.jsx
@@ -1,64 +1,64 @@
-// "use client";
+"use client";
 
 
-// import { Button } from '@/components/ui/button';
-// import useFetch from '@/hooks/use-fetch';
-// import { scanreceipt } from '@/server/transaction';
-// import { Camera, Loader2 } from 'lucide-react';
-// import React, { useEffect, useRef } from 'react'
-// import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
+import useFetch from '@/hooks/use-fetch';
+import { scanReceipt } from '@/server/transaction';
+import { Camera, Loader2 } from 'lucide-react';
+import React, { useEffect, useRef } from 'react'
+import { toast } from 'sonner';
 
-// const RecieptScanner = ({ onScanComplete }) => {
+const RecieptScanner = ({ onScanComplete }) => {
 
-//     const fileInputRef = useRef();
+    const fileInputRef = useRef();
 
-//     const {
-//         loading: scanReceiptLoading,
-//         fn: scanReceiptFn,
-//         data: scanData,
-//     } = useFetch(scanreceipt)
+    const {
+        loading: scanReceiptLoading,
+        fn: scanReceiptFn,
+        data: scanData,
+    } = useFetch(scanReceipt)
 
-//     const handleReceiptScan = async(file) => {
-//         if(file.size > 5 * 1024 * 1024){
-//             toast.error("File size should be less than 5MB")
-//             return;
-//         }
-//         await scanReceiptFn(file);
-//     }
+    const handleReceiptScan = async(file) => {
+        if(file.size > 5 * 1024 * 1024){
+            toast.error("File size should be less than 5MB")
+            return;
+        }
+        await scanReceiptFn(file);
+    }
 
-//     useEffect(() =>{
-//         if(scanData && !scanReceiptLoading ){
-//             onScanComplete(scanData);
-//             toast.success("receipt scanned Successfully")
-//         }
-//     },[scanReceiptLoading, scanData])
-//   return (
-//     <div>
-//         <input type="file"  ref={fileInputRef} accept='image/' capture='environment' className='hidden'
-//         onChange={(e) => {
-//             const file = e.target.files?.[0];
-//             if(file) handleReceiptScan(file)
-//         }}
-//         />
-//         <Button type='button' variant='outline' className='w-full h-10 animate-gradient hover:opacity-90 transition-opacity text-white hover:text-white'
-//         onClick={() => fileInputRef.current?.click()}
-//         disabled={scanReceiptLoading}
-//         >
-//             {scanReceiptLoading ? (
-//                 <>
-//                 <Loader2 className='mr-2 animate-spin'/>
-//                 <span>Scanning Receipt</span>
-//                 </>
-//             ): (
-//                 <>
-//                 <Camera className='mr-2'/>
-//                 <span>Scan Receipt with AI</span>
-//                 </>
-//             )}
-//         </Button>
+    useEffect(() =>{
+        if(scanData && !scanReceiptLoading ){
+            onScanComplete(scanData);
+            toast.success("receipt scanned Successfully")
+        }
+    },[scanReceiptLoading, scanData])
+  return (
+    <div>
+        <input type="file"  ref={fileInputRef} accept='image/' capture='environment' className='hidden'
+        onChange={(e) => {
+            const file = e.target.files?.[0];
+            if(file) handleReceiptScan(file)
+        }}
+        />
+        <Button type='button' variant='outline' className='w-full h-10 cursor-pointer animate-gradient  hover:opacity-90 transition-opacity text-white hover:text-white'
+        onClick={() => fileInputRef.current?.click()}
+        disabled={scanReceiptLoading}
+        >
+            {scanReceiptLoading ? (
+                <>
+                <Loader2 className='mr-2 animate-spin'/>
+                <span>Scanning Receipt</span>
+                </>
+            ): (
+                <>
+                <Camera className='mr-2'/>
+                <span className='text-white'>Scan Receipt with AI</span>
+                </>
+            )}
+        </Button>
         
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default RecieptScanner
+export default RecieptScanner
